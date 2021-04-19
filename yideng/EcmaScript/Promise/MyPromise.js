97202e1c9c263954f7ec4ce799a92d9019260677
melonlly <melonlly@outlook.com>
2021-04-19 17:31:26 +0800

    promise

---
+++ yideng/EcmaScript/Promise/MyPromise.js
@@ -0,0 +1,91 @@
+// Promise 状态
+const STATE = {
+    PENDING: "PENDING", // 执行态
+    FULFILLED: "FULFILLED", // 完成态
+    REJECTED: "REJECTED", // 拒绝态
+};
+
+/*
+    promise 解决过程
+*/
+function promiseResolutionProcedure(newPromise, x, resolve, reject) {
+    // 判断 promise 对象
+    if (newPromise === x) {
+        return new TypeError(`TypeError`);
+    } else {
+        if (x instanceof MyPromise) {
+            // 状态判断
+            if(x.state === STATE.PENDING) {
+                x.then((data) => {
+                    promiseResolutionProcedure(
+                        newPromise,
+                        data,
+                        resolve,
+                        reject
+                    ); // 递归调用
+                }, reject);
+            } else {
+                x.state === STATE.FULFILLED && resolve(x.value)
+                x.state === STATE.REJECTED && reject(x.value)
+            }
+        } else {
+            // 判断 thenable 对象
+            if (
+                x !== null &&
+                (typeof x === "object" || typeof x === "function") &&
+                typeof x.then === "function"
+            ) {
+                x.then((data) => {
+                    promiseResolutionProcedure(
+                        newPromise,
+                        data,
+                        resolve,
+                        reject
+                    ); // 递归调用
+                }, reject);
+            } else {
+                resolve(x);
+            }
+        }
+    }
+}
+
+class MyPromise {
+    constructor(fn) {
+        this.state = STATE.PENDING; // 当前状态
+        this.value = undefined; // 当前值
+        this.resolvedCallbacks = []; // 执行列表
+        const resolve = (val) => {
+            // 初始化时，resolve(promise || thenable)
+            if (typeof val === "object" || typeof val === "function") {
+                promiseResolutionProcedure(this, val, resolve, reject)
+                return
+            }
+            // 模拟微任务
+            setTimeout(() => {
+                if (this.state === STATE.PENDING) {
+                    this.state = STATE.FULFILLED;
+                    this.value = val;
+                    this.resolvedCallbacks.map((callback) => callback(val)); // 依次执行
+                }
+            });
+        };
+        const reject = (reason) => {
+            this.state = STATE.REJECTED;
+            this.value = reason;
+        };
+        fn(resolve, reject);
+    }
+    then(onFulfilled = (val) => val) {
+        if (this.state === STATE.PENDING) {
+            // 返回新Promise（链式调用
+            const newPromise = new MyPromise((resolve, reject) => {
+                this.resolvedCallbacks.push(() => {
+                    const x = onFulfilled(this.value);
+                    promiseResolutionProcedure(newPromise, x, resolve, reject);
+                });
+            });
+            return newPromise;
+        }
+    }
+}
--- yideng/EcmaScript/Promise/README.md
+++ yideng/EcmaScript/Promise/README.md
@@ -13,5 +13,10 @@
 ## 实际案例
 
 1. 在 setTimeout 中去 resolve
-2. Promise 实例化后直接 resolve
-3. 
+2. Promise 实例化后直接执行同步 resolve
+3. 防止多次 resolve（仅第一次 resolve 生效）
+4. then 方法链式调用（支持空调用）
+5. then 传递 thenable 对象
+6. then 传递 promise 对象
+7. resolve 传递 promise 对象
+8. 循环promise
